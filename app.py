0f5f3a707b6f6154c14c97eb686d626adc290252
LimKyounghee <37564259+LimKyounghee@users.noreply.github.com>
2020-08-25 11:12:36 +0900

    8/25

--- app.py
+++ app.py
@@ -112,14 +112,14 @@ def upload_file():
             'khwkurs': str(KHWKURS),#'1189.1000',  # 환율
             'setlType': '06',
             'dzterm': dzterm, #'20200807',
-            'yjqty': str(file_data['YJQTY']),#'1330.0000',  # 금융상품 단위수 - 주수
+            'yjqty': str(file_data['YJQTY']).replace(',',''),#'1330.0000',  # 금융상품 단위수 - 주수
             'yjamt': str(file_data['YJAMT']).replace(',',''),#'4163133.15',  # 약정금액(외화)
             'idxamt': str(file_data['IDXAMT']),#'31301753.01',  # 약정단가 (외화)
             'susuryo': str(file_data['SUSURYO']).replace(',',''),#'1665.25',  # 수수료(외화)
             'segum': str(file_data['SEGUM']),#'0',  # 세금 (외화)
             'fiamt': str(file_data['FIAMT']).replace(',','').replace('-',''),#'4164798.40',  # 정산금액(외화)
             'yjamtK': str(yjamtK) , #'4950381628',  # 약정금액(원화)
-            'idxamtK': str(idxamtK) ,#'3722091',  # 약정단가(원화)
+            'idxamtK': str(idxamtK) ,#'3722091',  # 약정단가(원화)p
             'susuryoK': str(susuryoK), #'1980148',  # 수수료(원화)
             'segumK': str(segumK),#'0',  # 세금(원화)
             'fiamtK': str(fiamtK),#'4952361776',  # 정산금액(원화)
